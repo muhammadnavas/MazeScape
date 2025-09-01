c4aa01d3972ea58c90e121d45bf13d5d14ec47c1
MrHidey <navasns0409@gmail.com>
2025-09-01 20:10:34 +0530

    Updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // config.js
 window.__APP_CONFIG__ = {
-    BACKEND_URL: "https://mazescape.onrender.com/api" // Updated to your Render URL
+    BACKEND_URL:"https://mazescape.onrender.com/api" // Updated to your Render URL
 };
